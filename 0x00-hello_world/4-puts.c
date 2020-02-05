355170f885b47761140e9f34f07ffb7c60d81f09
Orange1783 <fredy.granados@gmail.com>
2020-02-05 20:15:59 +0000

    4 puts 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main() {
     puts ("\"Programming is like building a multilingual puzzle");
     return 0;
