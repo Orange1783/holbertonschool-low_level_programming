9ea8e8da9eeb4c34aa87e713da8f10a56f70c50a
Orange1783 <fredy.granados@gmail.com>
2020-02-05 22:38:14 +0000

    6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char ch;
+int in;
+long int li;
+float fl;
+
+ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(ch));
+ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(in));
+ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li)); 	
+ printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fl));
+return (0);
+
+}
