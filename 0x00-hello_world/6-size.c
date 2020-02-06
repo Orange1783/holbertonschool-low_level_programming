12521f5e0dee6af724c7dc30238f9c4df2d81f38
Orange1783 <fredy.granados@gmail.com>
2020-02-06 01:39:32 +0000

    6 size 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,17 @@
  */
 int main(void)
 {
-printf("Size of a char:%lubyte(s)\n", sizeof(char));
-printf("Size of an int:%lubyte(s)\n", sizeof(int));
-printf("Size of a long int:%lubyte(s)\n", sizeof(long int));
-printf("Size of a long long int:%lubyte(s)\n", sizeof(long long int));
-printf("Size of a float:%lubyte(s)\n", sizeof(float));
+
+  char ch;
+  int in;
+  long lo;
+  long long lin;
+  float fl;
+
+printf("Size of a char:%lubyte(s)\n", sizeof(ch));
+printf("Size of an int:%lubyte(s)\n", sizeof(in));
+printf("Size of a long int:%lubyte(s)\n", sizeof(lo));
+printf("Size of a long long int:%lubyte(s)\n", sizeof(lin));
+printf("Size of a float:%lubyte(s)\n", sizeof(fl));
 return (0);
 }
