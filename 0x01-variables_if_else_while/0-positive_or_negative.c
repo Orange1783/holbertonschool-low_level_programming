58141e95373f25b16d25a34d4c6e781c9ef2372e
Orange1783 <fredy.granados@gmail.com>
2020-02-06 18:39:20 +0000

    es for is

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,20 +8,19 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
 {
-printf("%d es positivo\n", n);
+printf("%d is positive\n", n);
 }
 
 else if (n == 0)
 {
-printf("%d es zero\n", n);
+printf("%d is zero\n", n);
 }
 
 else
 {
-printf("%d es negative\n", n);
+printf("%d is negative\n", n);
 }
 
 return (0);
